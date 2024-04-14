aed278c3c06222d1568680b31fb6b03635d1fd67
Halisson da Silva Jesus <halissonsj@icloud.com>
2024-04-14 10:56:12 -0300

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Halisson da Silva Jesus on 14/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
